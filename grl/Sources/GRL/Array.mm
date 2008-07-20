25ff6dbeb80e0230741bf1b118288aeca1ece275
spootnik <spootnik@5929efdd-1752-0410-9c88-dedca7d919e8>
2008-07-20 16:15:13 +0000

    Added GArray implementation.

--- grl/Sources/GRL/Array.mm
+++ grl/Sources/GRL/Array.mm
@@ -49,22 +49,26 @@ struct _GRawArrayOpaqueData {
 	[super dealloc];
 }
 
-+ (id) arrayWithObjects:(const void **)objList
-{
-	return [[[self alloc] initWithObjects:objList] autorelease]; 
+- (id) init {
+	return [self initWithObjects:nil];
 }
 
-+ (id) arrayWithObjectsFromArray:(GRawArray *)list
++ (GRawArray *) arrayWithObjects:(const void **)objList
 {
-	return [self arrayWithObjectFromArray:list range:GMakeRange(0, [list count])];
+	return [[(GRawArray *)[self alloc] initWithObjects:objList] autorelease]; 
 }
 
-+ (id) arrayWithObjectsFromArray:(GRawArray *)list range:(GRange)range
++ (GRawArray *) arrayWithObjectsFromArray:(GRawArray *)list
 {
-	return [[[self alloc] initWithObjectFromArray:list range:range] autorelease];
+	return [self arrayWithObjectsFromArray:list range:GMakeRange(0, [list count])];
 }
 
-- (id) initWithObjects:(const void **)objList
++ (GRawArray *) arrayWithObjectsFromArray:(GRawArray *)list range:(GRange)range
+{
+	return [[(GRawArray *)[self alloc] initWithObjectsFromArray:list range:range] autorelease];
+}
+
+- (GRawArray *) initWithObjects:(const void **)objList
 {
 	self = [super init];
 	
@@ -90,21 +94,18 @@ struct _GRawArrayOpaqueData {
 			return self;
 		}
 		
-		unsigned i = 0;
-		while (objList[i]) {
-			mbs->vec->insert(mbs->vec->begin() + mbs->vec->size(), 1, objList[i]);
-		}
+		[self addObjects:objList];
 	}
 	
 	return self;
 }
 
-- (id) initWithObjectsFromArray:(GRawArray *)list
+- (GRawArray *) initWithObjectsFromArray:(GRawArray *)list
 {
 	return [self initWithObjectsFromArray:list range:GMakeRange(0, [list count])];
 }
 
-- (id) initWithObjectsFromArray:(GRawArray *)list range:(GRange)range
+- (GRawArray *) initWithObjectsFromArray:(GRawArray *)list range:(GRange)range
 {
 	self = [super init];
 	
@@ -125,50 +126,384 @@ struct _GRawArrayOpaqueData {
 			return nil;
 		}
 		
-		if (!list || range.start > [list count] || range.length == 0) {
-			GErrorSet (GInconsistencyError);
-			return self;
-		}
-		
-		if (range.start + range.length > [list count]) {
-			range.length = [list count] - range.start;
-		}
-		
-		for (unsigned i = range.start; i < range.start + range.length;i++) {
-			mbs->vec->insert(mbs->vec->begin() + mbs->vec->size(), 1, [list objectAtIndex:i]);
-		}
+		[self addObjectsFromArray:list range:range];
 	}
 	
 	return self;
 }
 
-//- (void) addObject:(const void *)obj;
-//- (void) addObject:(const void *)obj ifAbsent:(BOOL)flag;
-//- (void) insertObject:(const void *)obj atIndex:(unsigned)idx;
-//
-//- (void) addObjects:(const void **)list;
-//- (void) addObjectsFromArray:(GRawArray *)list;
-//- (void) addObjectsFromArray:(GRawArray *)list range:(GRange)range;
-//- (void) insertObjectsFromArray:(GRawArray *)list range:(GRange)range atIndex:(unsigned)index;
-//
-//- (void) removeObjectAtIndex:(unsigned)idx;
-//- (void) removeObject:(const void *)obj;
-//- (void) removeObjectsInRange:(GRange)range;
-//- (void) removeFirst;
-//- (void) removeLast;
-//- (void) empty;
-//
-//- (const void *) first;
-//- (const void *) last;
-//- (const void *) objectAtIndex:(unsigned)idx;
-//- (unsigned) indexOfObject:(const void *)obj;
-//- (BOOL) contains:(const void *)obj;
-//
-//- (unsigned) count;
+- (void) addObject:(const void *)obj
+{
+	if (obj) mbs->vec->insert(mbs->vec->end(), 1, obj);
+}
+
+- (void) addObject:(const void *)obj ifAbsent:(BOOL)flag
+{
+	if (!flag && [self contains:obj]) {
+		return;
+	}
+	
+	[self addObject:obj];
+}
+
+- (void) insertObject:(const void *)obj atIndex:(unsigned)idx
+{
+	if (obj) mbs->vec->insert(mbs->vec->begin() + idx, 1, obj);
+}
+
+
+- (void) addObjects:(const void **)list
+{
+	if (!list) {
+		GErrorSet (GInconsistencyError);
+		return;
+	}
+	
+	unsigned i = 0;
+	while (list[i]) {
+		mbs->vec->insert(mbs->vec->end(), 1, list[i]);
+	}
+}
+
+- (void) addObjectsFromArray:(GRawArray *)list
+{
+	[self addObjectsFromArray:list range:GMakeRange(0, [list count])];
+}
+
+- (void) addObjectsFromArray:(GRawArray *)list range:(GRange)range
+{
+	[self insertObjectsFromArray:list range:range atIndex:[list count]];
+}
+
+- (void) insertObjectsFromArray:(GRawArray *)list range:(GRange)range atIndex:(unsigned)index
+{
+	if (!list || range.start > [list count] || range.length == 0) {
+		GErrorSet (GInconsistencyError);
+		return;
+	}
+	
+	if (range.start + range.length > [list count]) {
+		range.length = [list count] - range.start;
+	}
+	
+	if (index > [self count])
+		index = [self count];
+	
+	for (unsigned i = range.start; i < range.start + range.length;i++) {
+		mbs->vec->insert(mbs->vec->begin() + index + (i - range.start), 1, [list objectAtIndex:i]);
+	}
+}
+
+
+- (void) removeObjectAtIndex:(unsigned)idx
+{
+	mbs->vec->erase(mbs->vec->begin() + idx);
+}
+
+- (void) removeObject:(const void *)obj
+{
+	unsigned idx = [self indexOfObject:obj];
+	if (idx != GNotFound) {
+		mbs->vec->erase(mbs->vec->begin() + idx);
+	} else {
+		GErrorSet (GInconsistencyError);
+	}
+}
+
+- (void) removeObjectsInRange:(GRange)range
+{
+	mbs->vec->erase(mbs->vec->begin() + range.start, mbs->vec->begin() + range.start + range.length);
+}
+
+- (void) removeFirst
+{
+	mbs->vec->erase(mbs->vec->begin());
+}
+
+- (void) removeLast
+{
+	mbs->vec->erase(mbs->vec->end());
+}
+
+- (void) empty
+{
+	mbs->vec->clear();
+}
+
+
+- (const void *) first
+{
+	return mbs->vec->at(0);
+}
+
+- (const void *) last
+{
+	return mbs->vec->at(mbs->vec->size());
+}
+
+- (const void *) objectAtIndex:(unsigned)idx
+{
+	return mbs->vec->at(idx);
+}
+
+- (unsigned) indexOfObject:(const void *)obj
+{
+	unsigned idx = GNotFound;
+	
+	if (obj) {
+		unsigned s = [self count];
+		for (unsigned i = 0;i < s;i++) {
+			if ([self objectAtIndex:i] == obj) {
+				idx = i;
+				break;
+			}
+		}
+	}
+	
+	return idx;
+}
+
+- (BOOL) contains:(const void *)obj
+{
+	if ([self indexOfObject:obj] == GNotFound)
+		return NO;
+	else
+		return YES;
+}
+
+
+- (unsigned) count
+{
+	return mbs->vec->size();
+}
 
 @end
 
 @implementation GArray
 
+- (void) dealloc
+{
+	[self empty];
+	[arr release];
+	[super dealloc];
+}
+
+- (id) init
+{
+	return [self initWithObjects:nil];
+}
+
++ (GArray *) arrayWithObjects:(id <GObject> *)objList
+{
+	return [[(GArray *)[self alloc] initWithObjects:objList] autorelease];
+}
+
++ (GArray *) arrayWithObjectsFromArray:(GArray *)list
+{
+	return [[(GArray *)[self alloc] initWithObjectsFromArray:list] autorelease];
+}
+
++ (GArray *) arrayWithObjectsFromArray:(GArray *)list range:(GRange)range
+{
+	return [[(GArray *)[self alloc] initWithObjectsFromArray:list range:range] autorelease];
+}
+
+
+- (GArray *) initWithObjects:(id <GObject> *)objList
+{
+	self = [super init];
+	
+	if (self) {
+		arr = [GRawArray new];
+		
+		if (!arr) {
+			GErrorSet (GMemError);
+			[self release];
+			return nil;
+		}
+		
+		[self addObjects:objList];
+	}
+	
+	return self;
+}
+
+- (GArray *) initWithObjectsFromArray:(GArray *)list
+{
+	self = [super init];
+	
+	if (self) {
+		arr = [GRawArray new];
+		
+		if (!arr) {
+			GErrorSet (GMemError);
+			[self release];
+			return nil;
+		}
+		
+		[self addObjectsFromArray:list];
+	}
+	
+	return self;
+}
+
+- (GArray *) initWithObjectsFromArray:(GArray *)list range:(GRange)range
+{
+	self = [super init];
+	
+	if (self) {
+		arr = [GRawArray new];
+		
+		if (!arr) {
+			GErrorSet (GMemError);
+			[self release];
+			return nil;
+		}
+		
+		[self addObjectsFromArray:list range:range];
+	}
+	
+	return self;
+}
+
+- (void) addObject:(id <GObject>)obj
+{
+	if (obj) {
+		[obj retain];
+		[arr addObject:obj];
+	}
+}
+
+- (void) addObject:(id <GObject>)obj ifAbsent:(BOOL)flag
+{
+	if (!flag && [self contains:obj]) {
+		return;
+	}
+	
+	[self addObject:obj];
+}
+
+- (void) insertObject:(id <GObject>)obj atIndex:(unsigned)idx
+{
+	if (obj) {
+		[obj retain];
+		[arr insertObject:obj atIndex:idx];
+	}
+}
+
+- (void) addObjects:(id <GObject> *)list
+{
+	if (!list)
+		return;
+	
+	for (unsigned i = 0;list[i];i++) {
+		[list[i] retain];
+		[arr addObject:list[i]];
+	}
+}
+
+- (void) addObjectsFromArray:(GArray *)list
+{
+	[self addObjectsFromArray:list range:GMakeRange(0, [list count])];
+}
+
+- (void) addObjectsFromArray:(GArray *)list range:(GRange)range
+{
+	[self insertObjectsFromArray:list range:range atIndex:[list count]];
+}
+
+- (void) insertObjectsFromArray:(GArray *)list range:(GRange)range atIndex:(unsigned)index
+{
+	if (!list || range.start > [list count] || range.length == 0) {
+		GErrorSet (GInconsistencyError);
+		return;
+	}
+	
+	if (range.start + range.length > [list count]) {
+		range.length = [list count] - range.start;
+	}
+	
+	if (index > [self count])
+		index = [self count];
+	
+	for (unsigned i = range.start; i < range.start + range.length;i++) {
+		[[list objectAtIndex:i] retain];
+		[arr insertObject:[list objectAtIndex:i] atIndex:index + (i - range.start)];
+	}
+}
+
+- (void) removeObjectAtIndex:(unsigned)idx
+{
+	[[self objectAtIndex:idx] release];
+	[arr removeObjectAtIndex:idx];
+}
+
+- (void) removeObject:(id <GObject>)obj
+{
+	unsigned idx = GNotFound;
+	if (GNotFound != (idx = [self indexOfObject:obj])) {
+		[self removeObjectAtIndex:idx];
+	}
+}
+
+- (void) removeObjectsInRange:(GRange)range
+{
+	if (range.start < [self count] && range.length > 0) {
+		if (range.start + range.length > [self count])
+			range.length = [self count] - range.start;
+		
+		for (unsigned i = range.start;i < range.start + range.length;i++) {
+			[self removeObjectAtIndex:range.start];
+		}
+	}
+}
+
+- (void) removeFirst
+{
+	[self removeObjectAtIndex:0];
+}
+
+- (void) removeLast
+{
+	[self removeObjectAtIndex:[self count]];
+}
+
+- (void) empty
+{
+	[self removeObjectsInRange:GMakeRange(0, [self count])];
+}
+
+- (id <GObject>) first
+{
+	return [self objectAtIndex:0];
+}
+
+- (id <GObject>) last
+{
+	return [self objectAtIndex:[self count]];
+}
+
+- (id <GObject>) objectAtIndex:(unsigned)idx
+{
+	return (id <GObject>)[arr objectAtIndex:idx];
+}
+
+- (unsigned) indexOfObject:(id <GObject>)obj
+{
+	return [arr indexOfObject:obj];
+}
+
+- (BOOL) contains:(id <GObject>)obj
+{
+	if (GNotFound != [self indexOfObject:obj])
+		return YES;
+	else
+		return NO;
+}
+
+- (unsigned) count
+{
+	return [arr count];
+}
 
 @end
