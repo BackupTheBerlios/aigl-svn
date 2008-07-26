2125cb737ce3b1959dab3648f29ddb15d0989eb5
spootnik <spootnik@5929efdd-1752-0410-9c88-dedca7d919e8>
2008-07-26 02:16:11 +0000

    Interfaced.

--- grl/Headers/GRL/Value.h
+++ grl/Headers/GRL/Value.h
@@ -26,14 +26,51 @@
  */
 
 #import <GRL/Object.h>
+#import <GRL/Config.h>
+
 
 @interface GValue : GObject {
-	void *buffer;
-	unsigned size;
+	void *data;
+	GType type;
 }
 
-+ (id)valueWithInt:(int)value;
-- (id)initWithInt:(int)value;
-- (int)intValue;
++ (id) valueWithInt8:(GInt8)value;
++ (id) valueWithUInt8:(GUInt8)value;
++ (id) valueWithInt16:(GInt16)value;
++ (id) valueWithUInt16:(GUInt16)value;
++ (id) valueWithInt32:(GInt32)value;
++ (id) valueWithUInt32:(GUInt32)value;
++ (id) valueWithFloat:(float)value;
++ (id) valueWithDouble:(double)value;
+
+- (id) initWithInt8:(GInt8)value;
+- (id) initWithUInt8:(GUInt8)value;
+- (id) initWithInt16:(GInt16)value;
+- (id) initWithUInt16:(GUInt16)value;
+- (id) initWithInt32:(GInt32)value;
+- (id) initWithUInt32:(GUInt32)value;
+- (id) initWithFloat:(float)value;
+- (id) initWithDouble:(double)value;
+
+- (void) setInt8Value:(GInt8)value;
+- (void) setUInt8Value:(GUInt8)value;
+- (void) setInt16Value:(GInt16)value;
+- (void) setUInt16Value:(GUInt16)value;
+- (void) setInt32Value:(GInt32)value;
+- (void) setUInt32Value:(GUInt32)value;
+- (void) setFloatValue:(float)value;
+- (void) setDoubleValue:(double)value;
+
+- (GInt8) int8Value;
+- (GUInt8) unsignedInt8Value;
+- (GInt16) int16Value;
+- (GUInt16) unsignedInt16Value;
+- (GInt32) int32Value;
+- (GUInt32) unsignedInt32Value;
+- (float) floatValue;
+- (double) doubleValue;
+
+- (GType) type;
 
 @end
+
