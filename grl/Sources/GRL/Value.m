c53472f2a0def537d25d9a42c0d56e3f27c80b51
spootnik <spootnik@5929efdd-1752-0410-9c88-dedca7d919e8>
2008-07-26 02:15:23 +0000

    Implemented.

--- grl/Sources/GRL/Value.m
+++ grl/Sources/GRL/Value.m
@@ -31,36 +31,230 @@
 
 @implementation GValue
 
-+ (id)valueWithInt:(int)value
+- (id) init
 {
-	return [[[self alloc] initWithInt:value] autorelease];
+	return [self initWithInt8:0];
 }
 
-- (id)initWithInt:(int)value
+- (void) dealloc
+{
+	free (data);
+	[super dealloc];
+}
+
++ (id) valueWithInt8:(GInt8)value
+{
+	return [[[self alloc] initWithInt8:value] autorelease];
+}
+
++ (id) valueWithUInt8:(GUInt8)value
+{
+	return [[[self alloc] initWithUInt8:value] autorelease];
+}
+
++ (id) valueWithInt16:(GInt16)value
+{
+	return [[[self alloc] initWithInt16:value] autorelease];
+}
+
++ (id) valueWithUInt16:(GUInt16)value
+{
+	return [[[self alloc] initWithUInt16:value] autorelease];
+}
+
++ (id) valueWithInt32:(GInt32)value
+{
+	return [[[self alloc] initWithInt32:value] autorelease];
+}
+
++ (id) valueWithUInt32:(GUInt32)value
+{
+	return [[[self alloc] initWithUInt32:value] autorelease];
+}
+
++ (id) valueWithFloat:(float)value
+{
+	return [[[self alloc] initWithFloat:value] autorelease];
+}
+
++ (id) valueWithDouble:(double)value
+{
+	return [[[self alloc] initWithDouble:value] autorelease];
+}
+
+- (id) initWithInt8:(GInt8)value
 {
 	self = [super init];
-	
-	if (self) {
-		size = sizeof(int);
-		buffer = GAllocate(size);
-		
-		int *tmp = (int *)buffer;
-		*tmp = value;
-	}
-	
+	if (self) [self setInt8Value:value];
 	return self;
 }
 
-- (int)intValue
+- (id) initWithUInt8:(GUInt8)value
 {
-	int ret = 0;
-	
-	if (buffer && size == sizeof(int)) {
-		int *val = (int *)buffer;
-		ret = *val;
-	}
-	
-	return ret;
+	self = [super init];
+	if (self) [self setUInt8Value:value];
+	return self;
 }
 
+- (id) initWithInt16:(GInt16)value
+{
+	self = [super init];
+	if (self) [self setInt16Value:value];
+	return self;
+}
+
+- (id) initWithUInt16:(GUInt16)value
+{
+	self = [super init];
+	if (self) [self setUInt16Value:value];
+	return self;
+}
+
+- (id) initWithInt32:(GInt32)value
+{
+	self = [super init];
+	if (self) [self setInt32Value:value];
+	return self;
+}
+
+- (id) initWithUInt32:(GUInt32)value
+{
+	self = [super init];
+	if (self) [self setUInt32Value:value];
+	return self;
+}
+
+- (id) initWithFloat:(float)value
+{
+	self = [super init];
+	if (self) [self setFloatValue:value];
+	return self;
+}
+
+- (id) initWithDouble:(double)value
+{
+	self = [super init];
+	if (self) [self setDoubleValue:value];
+	return self;
+}
+
+- (void) setInt8Value:(GInt8)value
+{
+	if (data) free (data);
+	data = GAllocInt8(value);
+	type = GInt8Type;
+}
+
+- (void) setUInt8Value:(GUInt8)value
+{
+	if (data) free (data);
+	data = GAllocUInt8(value);
+	type = GUInt8Type;
+}
+
+- (void) setInt16Value:(GInt16)value
+{
+	if (data) free (data);
+	data = GAllocInt16(value);
+	type = GInt16Type;
+}
+
+- (void) setUInt16Value:(GUInt16)value
+{
+	if (data) free (data);
+	data = GAllocUInt16(value);
+	type = GUInt16Type;
+}
+
+- (void) setInt32Value:(GInt32)value
+{
+	if (data) free (data);
+	data = GAllocInt32(value);
+	type = GInt32Type;
+}
+
+- (void) setUInt32Value:(GUInt32)value
+{
+	if (data) free (data);
+	data = GAllocUInt32(value);
+	type = GUInt32Type;
+}
+
+- (void) setFloatValue:(float)value
+{
+	if (data) free (data);
+	data = GAllocFloat(value);
+	type = GFloatType;
+}
+
+- (void) setDoubleValue:(double)value
+{
+	if (data) free (data);
+	data = GAllocDouble(value);
+	type = GDoubleType;
+}
+
+- (GInt8) int8Value
+{
+	GInt8 val = 0;
+	if (data && type == GInt8Type) val = *(GInt8 *)data;
+	return val;
+}
+
+- (GUInt8) unsignedInt8Value
+{
+	GUInt8 val = 0;
+	if (data && type == GUInt8Type) val = *(GUInt8 *)data;
+	return val;
+}
+
+- (GInt16) int16Value
+{
+	GInt16 val = 0;
+	if (data && type == GInt16Type) val = *(GInt16 *)data;
+	return val;
+}
+
+- (GUInt16) unsignedInt16Value
+{
+	GUInt16 val = 0;
+	if (data && type == GUInt16Type) val = *(GUInt16 *)data;
+	return val;
+}
+
+- (GInt32) int32Value
+{
+	GInt32 val = 0;
+	if (data && type == GInt32Type) val = *(GInt32 *)data;
+	return val;
+}
+
+- (GUInt32) unsignedInt32Value
+{
+	GUInt32 val = 0;
+	if (data && type == GUInt32Type) val = *(GUInt32 *)data;
+	return val;
+}
+
+- (float) floatValue
+{
+	float val = 0;
+	if (data && type == GFloatType) val = *(float *)data;
+	return val;
+}
+
+- (double) doubleValue
+{
+	double val = 0;
+	if (data && type == GDoubleType) val = *(double *)data;
+	return val;
+}
+
+- (GType) type
+{
+	return type;
+}
+
+
 @end
+
